da633bb35b94ceda5dd4be725b1378387e89102e
Thiago <tms1_tms2@hotmail.com>
2021-08-26 15:48:57 -0300

    useFoods hook

--- src/hooks/useFoods.tsx
+++ src/hooks/useFoods.tsx
@@ -1,27 +1,27 @@
 import { createContext, ReactNode, useContext, useState, useEffect } from "react";
 import api from "../services/api";
+import { Food } from "../types";
 
 interface FoodsProviderProps {
   children: ReactNode;
 }
 
-interface Food {
-  id: number;
-  name: string;
-  description: string;
-  price: number;
-  available: boolean;
-  image: string;
-}
+type FoodInput = Omit<Food, "id" | "available">;
 
 interface FoodsContextData {
   foods: Food[];
+  addFood: (food: FoodInput) => void;
+  editingFood: Food;
+  setEditingFood: (food: Food) => void;
+  editFood: (food: Food) => void;
+  deleteFood: (id: number) => void;
 }
 
 const FoodsContext = createContext<FoodsContextData>({} as FoodsContextData);
 
 export function FoodsProvider({ children }: FoodsProviderProps) {
-  const [foods, setFoods] = useState([]);
+  const [foods, setFoods] = useState<Food[]>([]);
+  const [editingFood, setEditingFood] = useState<Food>({} as Food);
 
   useEffect(() => {
     async function loadFoods() {
@@ -31,9 +31,50 @@ export function FoodsProvider({ children }: FoodsProviderProps) {
     loadFoods()
   }, []);
 
+  async function addFood(food: FoodInput) {
+    try {
+      const { data: newFood } = await api.post('/foods', {
+        ...food,
+        available: true,
+      });
+
+      setFoods([...foods, newFood]);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function editFood(food: Food) {
+    try {
+      const foodUpdated = await api.put(
+        `/foods/${editingFood.id}`,
+        { ...editingFood, ...food },
+      );
+
+      const foodsUpdated = foods.map(f =>
+        f.id !== foodUpdated.data.id ? f : foodUpdated.data,
+      );
+
+      setFoods(foodsUpdated);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function deleteFood(id: number) {
+    await api.delete(`/foods/${id}`);
+    const foodsFiltered = foods.filter(food => food.id !== id);
+    setFoods(foodsFiltered);
+  }
+
   return (
     <FoodsContext.Provider value={{
       foods,
+      addFood,
+      editingFood,
+      setEditingFood,
+      editFood,
+      deleteFood,
     }}>
       { children }
     </FoodsContext.Provider>
